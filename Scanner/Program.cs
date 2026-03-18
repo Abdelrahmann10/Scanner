ec4992457f1a2c6b03b0d42e60e9b8abaa83d7c5
Abdelrahman Sorour <abdosorour10@gmail.com>
2026-03-18 23:02:13 +0200

    Add project files.

---
+++ Scanner/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Scanner
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SourceCode Code = new SourceCode();
+            Tokenize(Code.sourceCode);
+        }
+
+        static void Tokenize(string code)
+        {
+            // Removing Multiple Lines Comment
+            code = Regex.Replace(code, @"/\*.*?\*/", "", RegexOptions.Singleline);
+            // Removing One line Comment
+            code = Regex.Replace(code, @"//.*", "");
+
+            // Tokens Def
+            var tokenDefinitions = new List<(string Name, string Pattern)>
+            {
+                ("KEYWORD", @"\b(int|main|if|else|return)\b"),
+                ("NUMBER", @"\d+(\.\d+)?"),
+                ("IDENTIFIER", @"\b[a-zA-Z_][a-zA-Z0-9_]*\b"),
+                ("OPERATOR", @"==|[=\-+*/]"),
+                ("SPECIALCHARACTER", @"[(){};,]")
+            };
+
+            // دمج كل الأنماط في Regex واحد
+            string fullPattern = "";
+            foreach (var def in tokenDefinitions)
+            {
+                fullPattern += $"(?<{def.Name}>{def.Pattern})|";
+            }
+            fullPattern = fullPattern.TrimEnd('|');
+
+            Regex regex = new Regex(fullPattern);
+            MatchCollection matches = regex.Matches(code);
+
+            // Printing The Specific Output Format
+            foreach (Match match in matches)
+            {
+                foreach (var def in tokenDefinitions)
+                {
+                    if (match.Groups[def.Name].Success)
+                    {
+                        Console.WriteLine($"<{def.Name}, {match.Value}>");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scanner/SourceCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scanner
+{
+    internal class SourceCode
+    {
+        public string sourceCode { get; set; } = @"
+                int main() {
+                int x, y;
+                // This is a single-line comment
+                if (x == 42) {
+                    /* This is
+                       a block
+                       comment */
+                    x = x - 3;
+                } else {
+                    y = 3.1; // Another comment
+                }
+                return 0;
+                }";
+    }
+}
